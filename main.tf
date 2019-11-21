c1aa8176c960ad5e2412aad0ccb5b2816051ddcd
Migara Ekanayake <mekanayake@paloaltonetworks.com>
2019-11-21 19:20:04 +0000

    panorama sec policy

--- main.tf
+++ main.tf
@@ -1,8 +1,57 @@
-resource "azurerm_resource_group" "test" {
-  name     = "migara_tfc_sec"
-  location = "North Europe"
 
-  tags = {
-    environment = "TFC"
+provider "panos" {
+  hostname = "40.85.87.187"
+}
+
+locals {
+  sec_rules = yamldecode(file("./firewall_rules.yml")).security_policy_rules
+}
+
+resource "panos_panorama_security_policy" "example" {
+
+  dynamic "rule" {
+    for_each = local.sec_rules
+
+    content {
+      name                  = rule.value.name
+      source_zones          = rule.value.source_zones
+      source_addresses      = rule.value.source_addresses
+      source_users          = rule.value.source_users
+      hip_profiles          = rule.value.hip_profiles
+      destination_zones     = rule.value.destination_zones
+      destination_addresses = rule.value.destination_addresses
+      applications          = rule.value.applications
+      services              = rule.value.services
+      categories            = rule.value.categories
+      action                = rule.value.action
+    }
   }
+
+  # rule {
+  #   name                  = "allow bizdev to dmz"
+  #   source_zones          = ["bizdev"]
+  #   source_addresses      = ["any"]
+  #   source_users          = ["any"]
+  #   hip_profiles          = ["any"]
+  #   destination_zones     = ["dmz"]
+  #   destination_addresses = ["any"]
+  #   applications          = ["any"]
+  #   services              = ["application-default"]
+  #   categories            = ["any"]
+  #   action                = "allow"
+  # }
+
+  # rule {
+  #   name                  = "deny sales to eng"
+  #   source_zones          = ["sales"]
+  #   source_addresses      = ["any"]
+  #   source_users          = ["any"]
+  #   hip_profiles          = ["any"]
+  #   destination_zones     = ["eng"]
+  #   destination_addresses = ["any"]
+  #   applications          = ["any"]
+  #   services              = ["application-default"]
+  #   categories            = ["any"]
+  #   action                = "deny"
+  # }
 }
